7bdf66dd3f70c82170675307abc8683e093f1feb
kmsilva00 <107791397+kmsilva00@users.noreply.github.com>
2022-09-16 17:19:44 +0200

    007

--- 8kyu/main.py
+++ 8kyu/main.py
@@ -1,10 +1,14 @@
-arr = [1,2,3,4,6,7,8]
+"""
 
-def first_non_consecutive(arr):
-    for i in range(0,(len(arr)-1)):
-        if arr[i] == arr[i+1]-1:
-            continue
-        return arr[i+1]
-    return None
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated,
+our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
 
-print(first_non_consecutive(arr))
+Return True if yes, False otherwise :)
+
+"""
+
+def hero(bullets, dragons):
+
+    if bullets / 2 >= dragons:
+        return True
+    return False
