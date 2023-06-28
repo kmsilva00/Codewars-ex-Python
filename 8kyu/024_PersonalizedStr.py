660b4ee2b18ce82198f411281c828055c7a46002
kmsilva00 <107791397+kmsilva00@users.noreply.github.com>
2023-06-28 14:26:57 +0200

    025

--- 8kyu/024_PersonalizedStr.py
+++ 8kyu/024_PersonalizedStr.py
@@ -1,3 +1,7 @@
 def greet(name, owner):
     if name == owner: return "Hello boss" 
     else : return "Hello guest" 
+    
+    
+def greetsol(name, owner):
+    return "Hello boss" if name == owner else "Hello guest"
